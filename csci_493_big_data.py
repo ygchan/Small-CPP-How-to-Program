f4a9f2efc1f7934277c7a309d14b76b6038601af
George Chan <ygchan89@gmail.com>
2019-10-16 22:11:44 -0400

    Add a file

---
+++ csci_493_big_data.py
@@ -0,0 +1,12 @@
+# Install Cassandra
+
+# Step 01. Download Java 
+# https://www.oracle.com/technetwork/java/javase/downloads/jdk13-downloads-5672538.html
+
+# Step 02. Download Cassandra
+# http://www.apache.org/dyn/closer.lua/cassandra/3.11.4/apache-cassandra-3.11.4-bin.tar.gz
+# Did not have curl or yam installed.
+
+# Step 03. Download Python Client
+# https://github.com/datastax/python-driver
+# There are option to allow faster building (instead of 10+ minutes)
